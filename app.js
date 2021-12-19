df73c15d3b916a42406227ec4afa71232e9d3b04
Dante Taviantz <taviantz@gmail.com>
2021-12-18 18:47:52 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)
+
+// const add = require('./utils.js')
+
+// const sum = add(4,-2)
+
+// console.log(sum) 
---
+++ utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js')
+
+const name = 'Mike'
+// what does deprecated mean?
+
+const add = function (a, b) {
+    return a + b
+}
+
+module.exports = add
